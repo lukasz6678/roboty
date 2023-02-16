c52f6dcdd9112c5facfdd4ebe8d859aba094f714
lukasz6678 <121644447+lukasz6678@users.noreply.github.com>
2023-02-16 12:14:44 +0100

    first commit

---
+++ src/Components/Searchbox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Searchbox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="tc">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots..."
+        onChange={searchChange}
+      ></input>
+    </div>
+  );
+};
+
+export default Searchbox;
---
+++ src/Containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import Searchbox from "../Components/Searchbox";
+import Scroll from "../Components/Scroll";
+import ErrorBoundry from "../Components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>ROBOFRIENDS</h1>
+          <Searchbox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
